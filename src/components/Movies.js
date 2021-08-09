6bd39efabfefc97f470a64859cb1ab97609c25bd
Eric <eric.m.lombardo@gmail.com>
2021-08-09 17:33:35 -0400

    Build movie app using routes and components

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,17 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+    
+        {movies.map(movie => {
+          return (<div key={movie.title} className="movie-card">
+            <h1>Name: {movie.title}</h1>
+            <p>Time: {movie.time}</p>
+            <ul>Genres: 
+              {movie.genres.map(genre => <li>{genre}</li>)}
+            </ul>
+          </div>)
+        })}
     </div>
   );
 };
